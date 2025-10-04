24abf04684cff311317fc2d1c06d313d1ffb66a4
Wasabee69 <130854883+Wasabee69@users.noreply.github.com>
2025-10-04 20:13:23 +0200

    Update: PA

--- NCPC2025/ProblemA.py
+++ NCPC2025/ProblemA.py
@@ -6,21 +6,17 @@
 
 def solve(s):
     s = int(s)
-    for i in range(1, 1000):
-        a = i
-        b = s-i
-        if a+b == s and a != 0 and b != 0 and -1000 < b  < 1000:
-            return f"{a} {b}"
-        a = -i
-        b = s+i
-        if a+b == s and a != 0 and b != 0 and -1000 < b < 1000:
-            return f"{a} {b}"
-    return f"weird"
+    if s == -1:
+        return f"{s+2} {-2}"
+    if s < 999:
+        return f"{s+1} {-1}"
+    return f"{s-1} {1}"
             
     
 
 test = input()
 print(solve(test))
+
 
 
 
